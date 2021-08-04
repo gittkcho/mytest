b6a83a24decff2084d87b31697719e39ce45ec57
Taka Cho <takamune.cho@att.com>
2021-08-04 14:02:40 -0400

    add groovy script

---
+++ docker-tag.groovy
@@ -0,0 +1,11 @@
+println 'Project version: ' + project.properties['dist.project.version'];
+def versionArray;
+if ( project.properties['dist.project.version'] != null ) {
+    versionArray = project.properties['dist.project.version'].split('-');
+}
+if ( project.properties['dist.project.version'].endsWith("-SNAPSHOT") ) {
+    project.properties['project.docker.latesttag.version']=versionArray[0] + "-SNAPSHOT-latest";
+} else {
+    project.properties['project.docker.latesttag.version']=versionArray[0] + "-STAGING-latest";
+}
+println 'New tag for docker: ' + project.properties['project.docker.latesttag.version'];
