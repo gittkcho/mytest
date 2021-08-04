f572dba5bc1920a19793f6a477dd4f28e47cf7e4
tkcho <takamune.cho@att.com>
2021-08-04 15:17:18 -0400

    add groovy script

--- docker-tag.groovy
+++ docker-tag.groovy
@@ -1,11 +1,13 @@
-println 'Project version: ' + project.properties['dist.project.version'];
-def versionArray;
-if ( project.properties['dist.project.version'] != null ) {
-    versionArray = project.properties['dist.project.version'].split('-');
+println 'Project version: ' + project.properties['dist.project.version']
+if (project.properties['dist.project.version'] != null) {
+    def versionArray = project.properties['dist.project.version'].split('-')
+    def minMaxVersionArray = versionArray[0].tokenize('.')
+    if (project.properties['dist.project.version'].endsWith("-SNAPSHOT")) {
+        project.properties['project.docker.latest.minmax.tag.version'] =
+             minMaxVersionArray[0] + "." + minMaxVersionArray[1] + "-SNAPSHOT-latest"
+     } else {
+        project.properties['project.docker.latest.minmax.tag.version'] =
+             minMaxVersionArray[0] + "." + minMaxVersionArray[1] + "-STAGING-latest"
+     }
+     println 'New tag for docker: ' + properties['project.docker.latest.minmax.tag.version']
 }
-if ( project.properties['dist.project.version'].endsWith("-SNAPSHOT") ) {
-    project.properties['project.docker.latesttag.version']=versionArray[0] + "-SNAPSHOT-latest";
-} else {
-    project.properties['project.docker.latesttag.version']=versionArray[0] + "-STAGING-latest";
-}
-println 'New tag for docker: ' + project.properties['project.docker.latesttag.version'];
