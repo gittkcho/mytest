2eb807ffe6507085ac90d9b65d897a65b937c7a1
tkcho <takamune.cho@att.com>
2021-08-04 14:35:17 -0400

    add groovy script

--- docker-tag.groovy
+++ docker-tag.groovy
@@ -1,11 +1,9 @@
-println 'HERE';
 println 'Project version: ' + project.properties['dist.project.version'];
 def versionArray;
 if ( project.properties['dist.project.version'] != null ) {
     versionArray = project.properties['dist.project.version'].split('-');
 }
 if ( project.properties['dist.project.version'].endsWith("-SNAPSHOT") ) {
-    println 'HERE0';
     project.properties['project.docker.latesttag.version']=versionArray[0] + "-SNAPSHOT-latest";
 } else {
     project.properties['project.docker.latesttag.version']=versionArray[0] + "-STAGING-latest";
