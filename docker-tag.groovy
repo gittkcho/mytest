ac2b4ed5bd2a0833b7098a7c95db3ff7500cc460
tkcho <takamune.cho@att.com>
2021-08-09 12:41:55 -0400

    add groovy script

--- docker-tag.groovy
+++ docker-tag.groovy
@@ -24,6 +24,7 @@ if (project.properties['dist.project.version'] != null) {
     def versionArray = project.properties['dist.project.version'].split('-')
     def minMaxVersionArray = versionArray[0].tokenize('.')
     if (project.properties['dist.project.version'].endsWith("-SNAPSHOT")) {
+println 'HERE'
         project.properties['project.docker.latest.minmax.tag.version'] =
              minMaxVersionArray[0] + "." + minMaxVersionArray[1] + "-SNAPSHOT-latest"
      } else {
